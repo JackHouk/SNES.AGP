fcc82caf4e100ffed36a2153a4bc6eadb5b2aa99
JackHouk <jackhouk@hotmail.com>
2016-04-26 17:10:12 -0400

    Created Globals, Interrupts Work
    Latch and Clock interrupts are now handled properly, global variables
    created for passing values between ASM driver calls.

--- SNES/SNES.ino
+++ SNES/SNES.ino
@@ -1,4 +1,6 @@
 #include <avr/interrupt.h>
+typedef unsigned char byte;
+byte regA, regB;
 // the setup function runs once when you press reset or power the board
 void setup() {
   //configure pins for proper I/O, 2 and 3 are input (latch and clock) Rx (0) is also left as input
@@ -12,10 +14,7 @@ void setup() {
     //set PORTD configuration (DDRD pins 0-7). 
     //NOTE: OUT is faster than STS when available
     "OUT 0x0A, R16\n"
-    //activate pull-up resistors on pins 2 & 3
-    "LDI R16, 0b00001100\n"
-    "OUT 0x0B, R16\n"
-    /*//disable pull-up resistors (need pull-down resistors wired to pin 2 and 3)
+    //disable pull-up resistors (need pull-down resistors wired to pin 2 and 3)
     "IN R17, 0x35\n"
     "LDI R16, 0b00010000\n"
     "OR R16, R17\n"
@@ -24,12 +23,11 @@ void setup() {
     "SEI\n"
     //configuration for EICRA
     //INT0 will interrupt on toggle, INT1 interrupts on rising edge
-    "LDI R16, 0b00001001\n"
+    "LDI R16, 0b00001101\n"
     "STS 0x69, R16\n"
     //enable interrupt on pin2 (INT0) and pin3 (INT1)
     "SBI 0x1D, 0\n"
     "SBI 0x1D, 1\n"
-    "LDI R16, 0\n"
     "pop R16\n"
     );
 }
@@ -45,14 +43,17 @@ void loop(){
     "WAIT_INT:\n"
       "NOP\n"
       "JMP WAIT_INT\n"
+     "pop R16\n"
+     "pop R17\n"
       );
 }
 
 //jumped to when SNES sends a latch pulse (INT0 Interrupt is triggered)
     ISR(INT0_vect){
+      
     asm volatile(
       "EXT_INT0:\n"
-        "SBI 0x0B, 7\n"/*
+        //"SBI 0x0B, 7\n"
         //check if latch is high or low, jump if high
         "SBIC 0x09, 2\n"
         "JMP LATCH_RISING_EDGE\n"
@@ -69,29 +70,33 @@ void loop(){
         "LDI R16, 0b10000000\n"
       //code if latch is on rising edge. Should be used to prep data for output, load things from memory, etc.
       "LATCH_RISING_EDGE:\n"
-      "CBI 0x0B, 7\n"
+        //"CBI 0x0B, 7\n"
         //set up R16 as bitmask for output
         "LDI R16, 0b10000000\n"
         //(NOT IMPLEMENTED)load values from memory for button presses
         //currently using ldi to load hard coded test values, 0 is pressed 1 is not pressed
-        "LDI R18, 0b00000000\n"
-        "LDI R19, 0b00000000\n"
+        "LDI R18, 0b10101011\n"
+        "LDI R19, 0b10100000\n"
+        "MOV %0, R18\n"
+        "MOV %1, R19\n"
+        :"=r"(regA),"=r"(regB)::
         //wait for next interrupt*/
       );};
       
     //jumpted to when SNES sends clock pulse. Should be used to output the next button press
     ISR(INT1_vect){
+      
     asm volatile(
       "EXT_INT1:\n"
-      "CBI 0x0B, 7\n"/*
+      //"CBI 0x0B, 7\n"
         //check if first 8 button presses are finished
+        //if they are, move the next 8 instructions to the R18 register
         "LDI R17, 0\n"
         "CPSE R17, R18\n"
-        //if they are, move the next 8 instructions to the R18 register
-        "MOV R18, R19\n"
+        "JMP LOAD_LAST_BUTTONS\n"
         //code if latch is on falling edge. Should be used to output the first bit to the SNES.
       "BUTTON_OUT:\n"
-      "CBI 0x0B, 7\n"
+        "CBI 0x0B, 7\n"
         //output first button press on falling edge of latch
         //AND output with bitmask
         "AND R16, R18\n"
@@ -103,5 +108,13 @@ void loop(){
         "OUT 0x0B, R16\n"
         //reset bitmask
         "LDI R16, 0b10000000\n"
-        //BUTTON_OUT returns and waits for next interrupt*/
+        "JMP EXIT\n"
+        
+        "LOAD_LAST_BUTTONS:\n"
+        "MOV R18, R19\n"
+        "JMP BUTTON_OUT\n"
+        "EXIT:\n"
+        "MOV %0, R18\n"
+        "MOV %1, R19\n"
+        :"=r"(regA),"=r"(regB)::
         );};
